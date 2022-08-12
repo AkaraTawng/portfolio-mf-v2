3ac5510c31f0b7ad9a4f135c9605ad493696e11e
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-12 07:23:46 +0700

    add ul and navlinks

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,18 @@
 import {NavLink} from "react-router-dom";
 
 export default function Nav(){
-    return <div>Nav</div>;
+    return <ul>
+        <li>
+            <NavLink activeClassName="active" exact to="/">Home</NavLink>
+        </li>
+        <li>
+            <NavLink activeClassName="active" exact to="/projects">Projects</NavLink>
+        </li>
+        <li>
+            <NavLink activeClassName="active" exact to="/resume">Resume</NavLink>
+        </li>
+        <li>
+            <NavLink activeClassName="active" exact to="/contact">Contact</NavLink>
+        </li>
+    </ul>;
 }
