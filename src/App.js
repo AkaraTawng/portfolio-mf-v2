c9606f66caa55e5a9b6b31bfa2d45da333f58def
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-12 07:41:58 +0700

    add route to contact in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Nav from "./components/Nav.js";
 import Contact from "./components/Contact.js";
 import Home from "./components/Home.js";
 import Projects from "./components/Projects.js";
 export default function App() {
   return<>
-  <BrowserRouter>
-  
-  </BrowserRouter>
+    <Nav/>
+    <Routes>
+      <Route path="/contact" element={<Contact/>}>
+        
+      </Route>
+    </Routes>
 </>
 };
