8e61f5f1a10059f3889127efb80fbef32553a3cc
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-12 07:06:04 +0700

    import Contact component into App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Nav from "./components/Nav.js";
+import Contact from "./components/Contact.js";
 export default function App() {
   return<>
   <BrowserRouter>
