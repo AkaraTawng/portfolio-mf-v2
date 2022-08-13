863832e8967025175ba474192b0ed974bade2f93
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-13 07:17:44 +0700

    add isLoading state to redirect component

--- src/components/RedirectPage.js
+++ src/components/RedirectPage.js
@@ -1,7 +1,9 @@
 
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 export default function RedirectPage() {
+    const [isLoading, setIsLoading] = useState(true);
     return useEffect(() => {
         window.location.replace("https://www.linkedin.com/feed/");
+        setIsLoading(false);
     },[]);
 }
