afa73e53339753d551a20dda05a4ad4e4585c76f
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-13 07:29:19 +0700

    import and conditionally render Loader in Redirect

--- src/components/RedirectPage.js
+++ src/components/RedirectPage.js
@@ -1,7 +1,9 @@
 
+import Loader from "./Loader";
 import {useEffect, useState} from "react";
 export default function RedirectPage() {
     const [isLoading, setIsLoading] = useState(true);
+    {isLoading && <Loader/>}
     return useEffect(() => {
         window.location.replace("https://www.linkedin.com/feed/");
         setIsLoading(false);
