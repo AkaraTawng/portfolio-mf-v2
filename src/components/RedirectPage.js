ad3a26bf795a7946b541f594ca4c8f9f851b5f51
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-13 19:13:41 +0700

    import useMatch and set match var

--- src/components/RedirectPage.js
+++ src/components/RedirectPage.js
@@ -1,8 +1,17 @@
 
+//hooks
+import { useEffect, useState } from "react";
+import { useMatch } from "react-router-dom";
+
+//pages
 import Loader from "./Loader";
-import {useEffect, useState} from "react";
+
 export default function RedirectPage() {
     const [isLoading, setIsLoading] = useState(true);
+    const match = useMatch();
+    console.log(match);
+
+
     {isLoading && <Loader/>}
     return useEffect(() => {
         window.location.replace("https://www.linkedin.com/feed/");
