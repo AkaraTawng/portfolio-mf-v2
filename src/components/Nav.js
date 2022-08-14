59d1f8ffff640a9939429d60e8bae1541eadef0c
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-15 06:39:33 +0700

    add nav-links-container div

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,26 @@
 import {NavLink} from "react-router-dom";
 
 export default function Nav(){
-    return <ul>
-        <li>
-            <NavLink activeClassName="active" to="/contact">Contact</NavLink>
-        </li>
-        <li>
-            <NavLink activeClassName="active" to="/home">Home</NavLink>
-        </li>
-        <li>
-            <NavLink activeClassName="active" to="/projects">Projects</NavLink>
-        </li>
-        <li>
-            <NavLink activeClassName="active" target="_blank" rel="noreferrer" to="/resume">Resume</NavLink>
-        </li>
-        <li>
-            <NavLink to="/linkedin" target="_blank" rel="noreferrer">Linkedin</NavLink>
-        </li>
-        <li>
-            <NavLink to="/github" target="_blank" rel="noreferrer">Github</NavLink>
-        </li>
-    </ul>;
+    return <div id="nav-links-container">
+        <ul>
+            <li>
+                <NavLink activeClassName="active" to="/contact">Contact</NavLink>
+            </li>
+            <li>
+                <NavLink activeClassName="active" to="/home">Home</NavLink>
+            </li>
+            <li>
+                <NavLink activeClassName="active" to="/projects">Projects</NavLink>
+            </li>
+            <li>
+                <NavLink activeClassName="active" target="_blank" rel="noreferrer" to="/resume">Resume</NavLink>
+            </li>
+            <li>
+                <NavLink to="/linkedin" target="_blank" rel="noreferrer">Linkedin</NavLink>
+            </li>
+            <li>
+                <NavLink to="/github" target="_blank" rel="noreferrer">Github</NavLink>
+            </li>
+        </ul>;
+    </div>
 }
