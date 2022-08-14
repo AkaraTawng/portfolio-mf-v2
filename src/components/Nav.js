860d9522ebeeb5e02c807e6ec8dfa5abdddc97c5
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-15 06:56:12 +0700

    add nav-link className to navLinks

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,22 +4,22 @@ export default function Nav(){
     return <div id="nav-links-container">
         <ul>
             <li>
-                <NavLink activeClassName="active" to="/contact">Contact</NavLink>
+                <NavLink className="nav-link" activeClassName="active" to="/contact">Contact</NavLink>
             </li>
             <li>
-                <NavLink activeClassName="active" to="/home">Home</NavLink>
+                <NavLink className="nav-link" activeClassName="active" to="/home">Home</NavLink>
             </li>
             <li>
-                <NavLink activeClassName="active" to="/projects">Projects</NavLink>
+                <NavLink className="nav-link" activeClassName="active" to="/projects">Projects</NavLink>
             </li>
             <li>
-                <NavLink activeClassName="active" target="_blank" rel="noreferrer" to="/resume">Resume</NavLink>
+                <NavLink className="nav-link" activeClassName="active" target="_blank" rel="noreferrer" to="/resume">Resume</NavLink>
             </li>
             <li>
-                <NavLink to="/linkedin" target="_blank" rel="noreferrer">Linkedin</NavLink>
+                <NavLink className="nav-link" to="/linkedin" target="_blank" rel="noreferrer">Linkedin</NavLink>
             </li>
             <li>
-                <NavLink to="/github" target="_blank" rel="noreferrer">Github</NavLink>
+                <NavLink className="nav-link" to="/github" target="_blank" rel="noreferrer">Github</NavLink>
             </li>
         </ul>;
     </div>
