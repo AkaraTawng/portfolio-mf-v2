d7235c9839a5e5da8b0523cbe9fd34f8fe16e581
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-15 07:06:53 +0700

    wrap nav ul in <nav>

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,30 @@
 import {NavLink} from "react-router-dom";
 
 export default function Nav(){
-    return <div id="nav-links-container">
-        <ul>
-            <li>
-                <NavLink className="nav-link" activeClassName="active" to="/contact">Contact</NavLink>
-            </li>
-            <li>
-                <NavLink className="nav-link" activeClassName="active" to="/home">Home</NavLink>
-            </li>
-            <li>
-                <NavLink className="nav-link" activeClassName="active" to="/projects">Projects</NavLink>
-            </li>
-            <li>
-                <NavLink className="nav-link" activeClassName="active" target="_blank" rel="noreferrer" to="/resume">Resume</NavLink>
-            </li>
-            <li>
-                <NavLink className="nav-link" to="/linkedin" target="_blank" rel="noreferrer">Linkedin</NavLink>
-            </li>
-            <li>
-                <NavLink className="nav-link" to="/github" target="_blank" rel="noreferrer">Github</NavLink>
-            </li>
-        </ul>
-    </div>
-}
+    return <>
+    
+        <div id="nav-links-container">
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink className="nav-link" activeClassName="active" to="/contact">Contact</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" activeClassName="active" to="/home">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" activeClassName="active" to="/projects">Projects</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" activeClassName="active" target="_blank" rel="noreferrer" to="/resume">Resume</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" to="/linkedin" target="_blank" rel="noreferrer">Linkedin</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="nav-link" to="/github" target="_blank" rel="noreferrer">Github</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </>}
