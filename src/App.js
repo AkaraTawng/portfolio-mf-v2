97345038d6c3afc36a824a43e81d5d0cbb85ba2c
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-16 08:58:20 +0700

    make loading state. conditionally render nav/loader based on loading state. show loader and hide nav when navigating to external site via redirect page

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import "../src/main.css";
 //router
 import { Routes, Route } from "react-router-dom";
 
+import {useState} from 'react';
+
 //components
 import Nav from "./components/Nav.js";
 import Contact from "./components/Contact.js";
 import Home from "./components/Home.js";
 import Projects from "./components/Projects.js";
 import RedirectPage from "./components/RedirectPage.js";
-// import Loader from "./components/Loader";
+import Loader from "./components/Loader";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
   return<>
-    <Nav/>
+  {isLoading ? <Loader/> : <Nav/>}
     <Routes>
       <Route path="/contact" element={<Contact/>}/>
       <Route path="/home" element={<Home/>} />
       <Route path="/projects" element={<Projects/>} />
       <Route path="/resume" element={<RedirectPage/>} />
-      <Route path="/linkedin" element={<RedirectPage/>} />
-      <Route path="/github" element={<RedirectPage/>} />
+      <Route path="/linkedin" element={<RedirectPage setIsLoading={setIsLoading}/>} />
+      <Route path="/github" element={<RedirectPage setIsLoading={setIsLoading}/>} />
     </Routes>
 </>
 };
