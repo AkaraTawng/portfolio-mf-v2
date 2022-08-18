de08c3c94188de234b27374f0bf10754d1bf31d2
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-18 16:23:42 +0700

    add label, checkbox input, and span in Theme component

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,4 +1,12 @@
 
 export default function Theme() {
     return <p id="theme-placeholder">Theme</p>
+    {/*
+
+    <label>
+        <input type="checkbox"/>
+        <span/>
+    </label> 
+
+*/}
 }
