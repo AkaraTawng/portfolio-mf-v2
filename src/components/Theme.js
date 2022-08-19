8716adceeb13372e997f3da7cf479b3f764bb488
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-19 09:02:59 +0700

    change slider bg color. change slider before color. make colors switch on checked

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,9 +1,6 @@
 
 export default function Theme() {
     return (<>
-    {/* <p id="theme-placeholder">Theme</p> */}
-
-
     <label className="toggle">
         <input type="checkbox"/>
         <span className="slider"/>
