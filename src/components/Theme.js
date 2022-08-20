ede5959da4dcb95f0d3a4f587f546f8307bfc598
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-20 19:58:12 +0700

    add toggle theme on change event to input

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,8 +1,12 @@
-
+import { useContext  } from "react"
+import { ThemeContext } from "./ThemeContext"
 export default function Theme() {
+    const theme = useContext(ThemeContext);
+    console.log("theme", theme)
+
     return (<>
     <label className="toggle">
-        <input type="checkbox"/>
+        <input onChange={() => theme.toggleTheme()}  type="checkbox"/>
         <span className="slider"/>
     </label> 
 </>)}
