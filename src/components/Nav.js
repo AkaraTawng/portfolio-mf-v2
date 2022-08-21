2d013a3804bf5708782d438de28202536534b861
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-21 15:03:33 +0700

    remove ul and and move into NavLinks component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,27 +4,6 @@ export default function Nav(){
     return <p id="nav-placeholder">Nav placeholder</p>
     return <>
         <div id="nav-wrapper">
-            <nav id="nav-links-container">
-                <ul>
-                    <li>
-                        <NavLink className="nav-link" activeClassName="active" to="/contact">Contact</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" activeClassName="active" to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" activeClassName="active" to="/projects">Projects</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" activeClassName="active" target="_blank" rel="noreferrer" to="/resume">Resume</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" to="/linkedin" target="_blank" rel="noreferrer">Linkedin</NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" to="/github" target="_blank" rel="noreferrer">Github</NavLink>
-                    </li>
-                </ul>
-            </nav>
+            
         </div>
     </>}
