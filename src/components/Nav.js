5240393d73e61939cc698f8c3c2cef9247ca9bf5
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-21 15:08:33 +0700

    import NavLinks into Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,11 @@
-import {NavLink} from "react-router-dom";
+import NavLinks from "./NavLinks.js";
 
 export default function Nav(){
     return <p id="nav-placeholder">Nav placeholder</p>
     return <>
         <div id="nav-wrapper">
-            
+            <nav>
+                <NavLinks />
+            </nav>
         </div>
     </>}
