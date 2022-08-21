fc61c5b2dc3cc851791ee4bf9bb01880a05eff3c
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-21 14:59:24 +0700

    add MobilNav component

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -2,7 +2,6 @@ import { useContext  } from "react"
 import { ThemeContext } from "./ThemeContext"
 export default function Theme() {
     const theme = useContext(ThemeContext);
-    console.log("theme", theme)
 
     return (<>
     <label className="toggle">
