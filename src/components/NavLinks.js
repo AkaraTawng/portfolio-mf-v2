be3188ae4153fcafd062460907458737e24ec466
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-21 15:02:38 +0700

    add NavLinks component

---
+++ src/components/NavLinks.js
@@ -0,0 +1,26 @@
+const NavLinks = () => {
+    return (<>
+            <ul>
+                <li>
+                    <NavLink className="nav-link" activeClassName="active" to="/contact">Contact</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" activeClassName="active" to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" activeClassName="active" to="/projects">Projects</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" activeClassName="active" target="_blank" rel="noreferrer" to="/resume">Resume</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" to="/linkedin" target="_blank" rel="noreferrer">Linkedin</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" to="/github" target="_blank" rel="noreferrer">Github</NavLink>
+                </li>
+            </ul>
+    </>)
+};
+
+export default NavLinks;
