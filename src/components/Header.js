e1834aed0e055598afb4be0bcbf6eb857b428325
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-21 14:56:52 +0700

    set h1 className to theme state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,15 @@
 import Nav from "./Nav.js";
 import Theme from "./Theme.js";
+
+/* test */
+import {useContext} from "react";
+import {ThemeContext} from "./ThemeContext.js";
+
+
+
 export default function Header(){
+    const context = useContext(ThemeContext);
+    console.log("theme", context)
     return <>
         <div id="header-wrapper">
             <div id="nav-theme-flex-container">    
@@ -8,7 +17,7 @@ export default function Header(){
                 <Theme/>
             </div>   
             <div id="header-title-subtitle-container">
-                <h1 id="header-title">Christopher Chamberlain</h1>
+                <h1 className={context.theme} id="header-title">Christopher Chamberlain</h1>
                 <h2>Front End Web Developer</h2>
             </div>            
         </div>
