b9e6a01e15986e1ec740cf3ab0c34b37a5dc6923
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-22 06:42:30 +0700

    make isOpen state in MobileNav.js

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,6 +1,8 @@
 import NavLinks from "./NavLinks.js";
 import { CgMenuMotion } from "react-icons/cg";
+import { useState } from "react";
 const MobileNav = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <nav className="mobile-nav">
             <CgMenuMotion className="hamburger"/>
