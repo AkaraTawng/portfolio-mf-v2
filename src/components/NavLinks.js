d5ad5e978176a798c126619504fdb674b60dfbfd
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-22 10:51:17 +0700

    remove activeClassName prop from all NavLinks - obsolete as of react-router v6

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,24 +1,25 @@
 import { NavLink } from "react-router-dom"; 
-const NavLinks = () => {
+const NavLinks = (props) => {
+    console.log("props", props)
     return (<>
             <ul id="nav-links-container">
                 <li>
-                    <NavLink className="nav-link" activeClassName="active" to="/contact">Contact</NavLink>
+                    <NavLink className="nav-link" to="/contact">Contact</NavLink>
                 </li>
                 <li>
-                    <NavLink className="nav-link" activeClassName="active" to="/">Home</NavLink>
+                    <NavLink className="nav-link" to="/">Home</NavLink>
                 </li>
                 <li>
-                    <NavLink className="nav-link" activeClassName="active" to="/projects">Projects</NavLink>
+                    <NavLink className="nav-link" to="/projects">Projects</NavLink>
                 </li>
                 <li>
-                    <NavLink className="nav-link" activeClassName="active" target="_blank" rel="noreferrer" to="/resume">Resume</NavLink>
+                    <NavLink className="nav-link" target="_blank" rel="noreferrer" to="/resume">Resume</NavLink>
                 </li>
                 <li>
-                    <NavLink className="nav-link" to="/linkedin" target="_blank" rel="noreferrer" activeClassName="active">Linkedin</NavLink>
+                    <NavLink className="nav-link" to="/linkedin" target="_blank" rel="noreferrer">Linkedin</NavLink>
                 </li>
                 <li>
-                    <NavLink className="nav-link" to="/github" target="_blank" rel="noreferrer" activeClassName="active">Github</NavLink>
+                    <NavLink className="nav-link" to="/github" target="_blank" rel="noreferrer">Github</NavLink>
                 </li>
             </ul>
     </>)
