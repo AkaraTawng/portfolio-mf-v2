fdcb8ad958494635b3f9dae2aca6636ae5a01081
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-23 05:50:24 +0700

    receive onCloseMobileMenu from props and pass to onClick for every li

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -3,22 +3,22 @@ const NavLinks = (props) => {
     console.log("props", props)
     return (<>
             <ul id="nav-links-container">
-                <li>
+                <li onClick={() => props.onCloseMobileMenu()}>
                     <NavLink className="nav-link" to="/contact">Contact</NavLink>
                 </li>
-                <li>
+                <li onClick={() => props.onCloseMobileMenu()}>
                     <NavLink className="nav-link" to="/">Home</NavLink>
                 </li>
-                <li>
+                <li onClick={() => props.onCloseMobileMenu()}>
                     <NavLink className="nav-link" to="/projects">Projects</NavLink>
                 </li>
-                <li>
+                <li onClick={() => props.onCloseMobileMenu()}>
                     <NavLink className="nav-link external-navlink" target="_blank" rel="noreferrer" to="/resume">Resume</NavLink>
                 </li>
-                <li>
+                <li onClick={() => props.onCloseMobileMenu()}>
                     <NavLink className="nav-link external-navlink" to="/linkedin" target="_blank" rel="noreferrer">Linkedin</NavLink>
                 </li>
-                <li>
+                <li onClick={() => props.onCloseMobileMenu()}>
                     <NavLink className="nav-link external-navlink" to="/github" target="_blank" rel="noreferrer">Github</NavLink>
                 </li>
             </ul>
