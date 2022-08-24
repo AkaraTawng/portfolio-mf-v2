90a82977c040eedfc0e114bae9d892ae677950a7
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-25 05:46:48 +0700

    convert Footer from arrow function to regular function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,28 @@
 
-const Footer = () => {
+export default function Footer() {
     return <>
-        <section id="sitemap">
-            <ul>
-                <li>Home</li>
-                <li>Projects</li>
-                <li>Contact</li>
-                <li>Resume</li>
-                <li>LinkedIn</li>
-                <li>GitHub</li>
-            </ul>
-        </section>
+        <div id="footer-wrapper">
+            <section id="sitemap">
+                <ul>
+                    <li>Home</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                    <li>Resume</li>
+                    <li>LinkedIn</li>
+                    <li>GitHub</li>
+                </ul>
+            </section>
 
-        <section id="contact">
-            <ul>
-                <li>
-                    <span>Phone: </span>+856 020-983-09152
-                </li>
-                <li>
-                    <span>Email: </span>c.r.chamberlain1997@gmail.com
-                </li>
-            </ul>
-        </section>
+            <section id="contact">
+                <ul>
+                    <li>
+                        <span>Phone: </span>+856 020-983-09152
+                    </li>
+                    <li>
+                        <span>Email: </span>c.r.chamberlain1997@gmail.com
+                    </li>
+                </ul>
+            </section>
+        </div>
     </>
 }
-
-export default { Footer };
