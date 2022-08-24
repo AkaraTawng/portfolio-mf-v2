02afa5aabe94186adb5cdbbf1bd901c459f4e801
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-25 06:04:11 +0700

    add sitemap-contact-flex-container div

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,27 +2,29 @@
 export default function Footer() {
     return <>
         <div id="footer-wrapper">
-            <section id="sitemap">
-                <ul>
-                    <li>Home</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                    <li>Resume</li>
-                    <li>LinkedIn</li>
-                    <li>GitHub</li>
-                </ul>
-            </section>
+             <div id="sitemap-contact-flex-container">   
+                <section id="sitemap">
+                    <ul>
+                        <li>Home</li>
+                        <li>Projects</li>
+                        <li>Contact</li>
+                        <li>Resume</li>
+                        <li>LinkedIn</li>
+                        <li>GitHub</li>
+                    </ul>
+                </section>
 
-            <section id="contact">
-                <ul>
-                    <li>
-                        <span>Phone: </span>+856 020-983-09152
-                    </li>
-                    <li>
-                        <span>Email: </span>c.r.chamberlain1997@gmail.com
-                    </li>
-                </ul>
-            </section>
+                <section id="contact">
+                    <ul>
+                        <li>
+                            <span>Phone: </span>+856 020-983-09152
+                        </li>
+                        <li>
+                            <span>Email: </span>c.r.chamberlain1997@gmail.com
+                        </li>
+                    </ul>
+                </section>
+            </div>
             <p id="copyright">&copy; Christopher Chamberlain</p>
         </div>
     </>
