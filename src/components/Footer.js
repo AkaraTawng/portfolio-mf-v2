0fe8bac3c3381155363e4fddcd00625bea60f915
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-25 05:30:33 +0700

    add sitemap and contact ul and lis

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,27 @@
 
 const Footer = () => {
     return <>
-        null;
+        <section id="sitemap">
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Contact</li>
+                <li>Resume</li>
+                <li>LinkedIn</li>
+                <li>GitHub</li>
+            </ul>
+        </section>
+
+        <section id="contact">
+            <ul>
+                <li>
+                    <span>Phone: </span>+856 020-983-09152
+                </li>
+                <li>
+                    <span>Email: </span>c.r.chamberlain1997@gmail.com
+                </li>
+            </ul>
+        </section>
     </>
 }
 
