ef5f1678da2282f6ba362fa57a9fd721751a7020
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-25 12:07:29 +0700

    add skills section and markup

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,5 +5,18 @@ export default function Home(){
             <p>I'm a meticulous front-end web developer with good knowledge of modern web development practices who can translate any design into a kick-ass website.
             </p>
         </section>
+
+        <section id="skills-section">
+            <div>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </section>
     </>;
 }
