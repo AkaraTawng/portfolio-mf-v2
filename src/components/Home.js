58f5e0e87320795eb5742dc484e52dc608edf91f
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-25 12:04:29 +0700

    add about me section and content markup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,9 @@
 export default function Home(){
-    return <div>Home</div>;
+    return <>
+        <section id="about-me-section">
+            <h2>About Me</h2>
+            <p>I'm a meticulous front-end web developer with good knowledge of modern web development practices who can translate any design into a kick-ass website.
+            </p>
+        </section>
+    </>;
 }
