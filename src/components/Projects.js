58313340d39b81abf4db833e707dfb9dcdc7f069
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-27 13:41:57 +0700

    add calculator pic to project item 1

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import ProjectItem from "./ProjectItem";
 export default function Projects(){
     return <>
-        <ProjectItem title={"Calculator"} image={require("../assets/office.jpg")} repo={"#"} site={"#"}/>
+        <ProjectItem title={"Calculator"} image={require("../assets/img/calculator.png")} repo={"#"} site={"#"}/>
     </>;
 }
