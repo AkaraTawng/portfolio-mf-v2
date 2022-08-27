ecd2cc545b1b20339e314e25fc3cd5ba554842ee
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-27 13:43:04 +0700

    render second product item for mws in Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,5 +2,7 @@ import ProjectItem from "./ProjectItem";
 export default function Projects(){
     return <>
         <ProjectItem title={"Calculator"} image={require("../assets/img/calculator.png")} repo={"#"} site={"#"}/>
+        
+        <ProjectItem title={"Man With a Spade"} image={require("../assets/img/mws.png")} repo={"#"} site={"#"}/>
     </>;
 }
