98b5c7c8a7bc20b1014e8578fbed952d13604c60
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-08-28 07:11:37 +0700

    pass repo links as repo props to project items

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,13 +2,13 @@ import { MdLockClock } from "react-icons/md";
 import ProjectItem from "./ProjectItem";
 export default function Projects(){
     return <>
-        <ProjectItem title={"Calculator"} image={require("../assets/img/calculator.png")} repo={"#"} site={"#"}/>
+        <ProjectItem title={"Calculator"} image={require("../assets/img/calculator.png")} repo={"https://github.com/AkaraTawng/Calculator"} site={"#"}/>
         
-        <ProjectItem title={"Man With a Spade"} image={require("../assets/img/mws.png")} repo={"#"} site={"#"}/>
+        <ProjectItem title={"Man With a Spade"} image={require("../assets/img/mws.png")} repo={"https://github.com/AkaraTawng/man-with-a-spade"} site={"#"}/>
         
-        <ProjectItem title={"Cactus Connoiseur"} image={require("../assets/img/cactus.png")} repo={"#"} site={"#"}/>
+        <ProjectItem title={"Cactus Connoiseur"} image={require("../assets/img/cactus.png")} repo={"https://github.com/AkaraTawng/product-landing-page"} site={"#"}/>
         
-        <ProjectItem title={"Exercise Survery Form"} image={require("../assets/img/survey.png")} repo={"#"} site={"#"}/>
+        <ProjectItem title={"Exercise Survery Form"} image={require("../assets/img/survey.png")} repo={"https://github.com/AkaraTawng/survey-form"} site={"#"}/>
         
         <ProjectItem title={"Income Tracker"} image={require("../assets/img/incomeblur4.png")} repo={"#"} viewrepo={<MdLockClock/>} viewsite={<MdLockClock/>} site={"#"}/>
     </>;
