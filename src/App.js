91ed7fe3a7d18f76f184ed7f733fbf931576e58f
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-09-02 16:29:29 +0700

    import useLocation and AnimatePresence. wrap routes with AnimatePresence. set location and key props on routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "../src/main.css";
 
 //router
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import {useState} from 'react';
+
 
 //components
 import Nav from "./components/Nav.js";
@@ -14,22 +15,26 @@ import RedirectPage from "./components/RedirectPage.js";
 import Loader from "./components/Loader";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { AnimatePresence } from "framer-motion";
 
 
 export default function App() {
   /* refactor loading state to context for resuse */
   const [isLoading, setIsLoading] = useState(false);
+  const location = useLocation();
   return<>
       {isLoading ? <Loader/> : <Header/>}
         
-        <Routes>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/" element={<Home/>} />
-          <Route path="/projects" element={<Projects/>} />
-          <Route path="/resume" element={<RedirectPage setIsLoading={setIsLoading}/>} />
-          <Route path="/linkedin" element={<RedirectPage setIsLoading={setIsLoading}/>} />
-          <Route path="/github" element={<RedirectPage setIsLoading={setIsLoading}/>} />
-        </Routes>
+        <AnimatePresence>
+          <Routes location={location} key={location.key}>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/" element={<Home/>} />
+            <Route path="/projects" element={<Projects/>} />
+            <Route path="/resume" element={<RedirectPage setIsLoading={setIsLoading}/>} />
+            <Route path="/linkedin" element={<RedirectPage setIsLoading={setIsLoading}/>} />
+            <Route path="/github" element={<RedirectPage setIsLoading={setIsLoading}/>} />
+          </Routes>
+        </AnimatePresence>
       <Footer/>
 </>
 };
