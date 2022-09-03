501b4c8fa47c2959b51f7cf6caf310f84879d525
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-09-03 19:41:52 +0700

    wrap #home-wrapper with AnimatedPage

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,45 +4,28 @@ import { useMatch } from "react-router-dom";
 import HomeTitle from "./HomeTitle.js";
 import ContactTitle from "./ContactTitle.js";
 import ProjectTitle from "./ProjectTitle.js";
-import { motion } from 'framer-motion';
 
-/* test */
-import {useContext} from "react";
-import {ThemeContext} from "./ThemeContext.js";
-
-export default function Header(){
+export default function Header({key}){
     const home = useMatch("/")
     const contact = useMatch("/contact")
     const project = useMatch("/projects");
-    const context = useContext(ThemeContext);
     let title = "";
     if (home) {
-        title =  <HomeTitle/>; 
+        title =  <HomeTitle key={key} />; 
     } else if (contact) {
-        title = <ContactTitle/>;
+        title = <ContactTitle key={key}/>;
     } else if (project) {
         title = <ProjectTitle/>
     }
-    console.log("theme", context)
+
     return <>
         <div id="header-wrapper">
             <div id="nav-theme-flex-container">    
                 <Nav/>
                 <Theme/>
             </div>   
-            <motion.div id="header-title-subtitle-container"
-            initial= {{opacity: 0, y: -500}}
-            animate={{opacity: 1, y: 0}}
-            transition = {{
-                delay: 0.3,
-                duration: 1,
-                type: 'spring',
-                bounce: 0.5,
-
-            }}
-
-            >
-               {title}
-            </motion.div>            
+                <div id="header-title-subtitle-container">
+                {title}
+                </div>        
         </div>
      </>};
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,48 +6,54 @@ import { FaGitAlt } from "react-icons/fa";
 import { SiReact } from "react-icons/si";
 import Tippy from "@tippyjs/react";
 import Tooltip from "./Tooltip";
+import { motion } from "framer-motion";
+import AnimatedPage from "./AnimatedPage";
 
 
 
 export default function Home(){
     return <>
-    <div id="home-wrapper">
-        <section id="about-me-section">
-            <h2>About Me</h2>
-            <p>I'm a meticulous front-end web developer with good knowledge of modern web development practices who can translate any design into a kick-ass website.
-            </p>
-        </section>
+    <AnimatedPage>
+        <div id="home-wrapper">
+            <section id="about-me-section"
+            
+            >
+                <h2>About Me</h2>
+                <p>I'm a meticulous front-end web developer with good knowledge of modern web development practices who can translate any design into a kick-ass website.
+                </p>
+            </section>
 
-        <section id="skills-section">
-            <h2>Skills</h2>
-            <div id="grid-container">
-                <ul>
-                    <Tippy content={<Tooltip content="HTML/HTML5"/>}>
-                        <li><AiFillHtml5 size="4rem"/></li>
-                    </Tippy>
+            <section id="skills-section">
+                <h2>Skills</h2>
+                <div id="grid-container">
+                    <ul>
+                        <Tippy content={<Tooltip content="HTML/HTML5"/>}>
+                            <li><AiFillHtml5 size="4rem"/></li>
+                        </Tippy>
 
-                    <Tippy content={<Tooltip content="CSS/CSS3"/>}>
-                    <li>< DiCss3 size="4rem"/></li>
-                    </Tippy>
+                        <Tippy content={<Tooltip content="CSS/CSS3"/>}>
+                        <li>< DiCss3 size="4rem"/></li>
+                        </Tippy>
 
-                    <Tippy content={<Tooltip content="JavaScript"/>}>
-                    <li><SiJavascript  size="3.5rem"/></li>
-                    </Tippy>
+                        <Tippy content={<Tooltip content="JavaScript"/>}>
+                        <li><SiJavascript  size="3.5rem"/></li>
+                        </Tippy>
 
-                    <Tippy content={<Tooltip content="SCSS"/>}>
-                    <li><DiSass  size="4rem"/></li>
-                    </Tippy>
+                        <Tippy content={<Tooltip content="SCSS"/>}>
+                        <li><DiSass  size="4rem"/></li>
+                        </Tippy>
 
-                    <Tippy content={<Tooltip content="Git/GitHub"/>}>
-                    <li><FaGitAlt  size="4rem"/></li>
-                    </Tippy>
+                        <Tippy content={<Tooltip content="Git/GitHub"/>}>
+                        <li><FaGitAlt  size="4rem"/></li>
+                        </Tippy>
 
-                    <Tippy content={<Tooltip content="ReactJS"/>}>
-                    <li><SiReact  size="4rem"/></li>
-                    </Tippy>
-                </ul>
-            </div>
-        </section>
-    </div>
+                        <Tippy content={<Tooltip content="ReactJS"/>}>
+                        <li><SiReact  size="4rem"/></li>
+                        </Tippy>
+                    </ul>
+                </div>
+            </section>
+        </div>
+    </AnimatedPage>
     </>;
 }
