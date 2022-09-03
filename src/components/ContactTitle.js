5669ed9bdc8daafb246c1bf166b93dbb138cb206
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-09-03 19:40:10 +0700

    wrap #contact title with AnimatedTitle

--- src/components/ContactTitle.js
+++ src/components/ContactTitle.js
@@ -1,3 +1,8 @@
+import AnimatedTitle from "./AnimatedTitle";
 export default function ContactTitle() {
-    return <h1 id="contact-title">Contact</h1>;
+    return <>
+    <AnimatedTitle>
+            <h1 id="contact-title">Contact</h1>
+    </AnimatedTitle>
+    </>
 };
