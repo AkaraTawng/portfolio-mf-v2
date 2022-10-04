b511d2ed94c825271cb8599e9a0979bf2170f234
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-10-04 10:21:29 +0700

    update cactus site link. add excercise form site link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,9 +17,9 @@ export default function Projects(props){
             
             <ProjectItem className="mws" title={"Man With a Spade"} image={require("../assets/img/mws.png")} repo={"https://github.com/AkaraTawng/man-with-a-spade"} site={"https://man-with-a-spade.netlify.app/"}/>
 
-            <ProjectItem className="cc" title={"Cactus Connoiseur"} image={require("../assets/img/cactus.png")} repo={"https://github.com/AkaraTawng/product-landing-page"} site={"https://cactus-connoiseur.netlify.app/"}/>
+            <ProjectItem className="cc" title={"Cactus Connoiseur"} image={require("../assets/img/cactus.png")} repo={"https://github.com/AkaraTawng/product-landing-page"} site={"https://cactus-connoisseur.netlify.app/"}/>
 
-            <ProjectItem className="sf" title={"Exercise Survey Form"} image={require("../assets/img/survey.png")} repo={"https://github.com/AkaraTawng/survey-form"} site={"#"}/>
+            <ProjectItem className="sf" title={"Exercise Survey Form"} image={require("../assets/img/survey.png")} repo={"https://github.com/AkaraTawng/survey-form"} site={"https://exercise-survey.netlify.app/"}/>
         </div>
             <ProjectItem className="it" title={"Income Tracker"} image={require("../assets/img/incomeblur4.png")} repo={"#"} viewrepo={<MdLockClock/>} viewsite={<MdLockClock/>} site={"#"}/>
     </AnimatedPage>
