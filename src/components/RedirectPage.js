031291f1fcbaa640d65875d3e6c3183ff8a14754
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-10-05 05:48:21 +0700

    update resume redirect link

--- src/components/RedirectPage.js
+++ src/components/RedirectPage.js
@@ -15,7 +15,7 @@ export default function RedirectPage(props) {
         } else if(matchGithub){
             window.location.replace("https://github.com/AkaraTawng");
         } else if(matchResume){
-            window.location.replace("#");
+            window.location.replace("https://drive.google.com/file/d/1Nh9CODUcthSiVENJJRx3UsQa9jnjltFW/view?usp=sharing");
         }
     },[]);   
 }
