0f07888d3c4b5ace50065435b96bf39ba7ec10f9
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-10-08 14:31:22 +0700

    update resume link to resume with new phone number

--- src/components/RedirectPage.js
+++ src/components/RedirectPage.js
@@ -15,7 +15,7 @@ export default function RedirectPage(props) {
         } else if(matchGithub){
             window.location.replace("https://github.com/AkaraTawng");
         } else if(matchResume){
-            window.location.replace("https://drive.google.com/file/d/1Nh9CODUcthSiVENJJRx3UsQa9jnjltFW/view?usp=sharing");
+            window.location.replace("https://drive.google.com/file/d/1JjpN6mrlSZ6-aiS4vt0cAcYP7Bx0Jfpo/view?usp=sharing");
         }
     },[]);   
 }
